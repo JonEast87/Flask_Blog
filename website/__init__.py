5c71bad1bbdf3fc4a33e7088a6765b19a1613461
JonEast87 <j.d.east@icloud.com>
2023-06-29 19:04:05 -0700

    established models.py for the user information needed for login and signup. as well as a basic sqlite database.

--- website/__init__.py
+++ website/__init__.py
@@ -3,10 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "Green Bean"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # Importing auth, views routes to __init__
     from .auth import auth
@@ -15,5 +20,19 @@ def create_app():
     # Registering the auth, views blueprint for the 'app' to utilize
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(views, url_prefix="/")
+
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    # Uses 'sessions' to store the user information to remember it for later
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
