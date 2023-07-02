f17c05d4ce17fcb5cec8bab86b38f0d83cbe7f96
JonEast87 <j.d.east@icloud.com>
2023-07-02 08:35:46 -0700

    finished user auth with the signup and login routes completed.

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, render_template, redirect, url_for, request
 from . import db
-from models import User
-from flask_login import login_user, logout_user, login_required
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
@@ -34,6 +34,7 @@ def signup():
             new_user = User(email=email, username=username, password1=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('User created!')
             return redirect(url_for('views.home'))
 
@@ -42,11 +43,25 @@ def signup():
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.moethod == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect.', category='error')
+        else:
+            flash('Email does not exist', category='error')
+
     return render_template("login.html")
 
 
+@login_required
 @auth.route("logout")
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
