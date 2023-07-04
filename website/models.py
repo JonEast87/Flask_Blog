4a32d8c55acc85cf8bdb497f1f320223b3f3b62e
JonEast87 <j.d.east@icloud.com>
2023-07-04 13:35:54 -0700

    created Comment model, added relations to User and Post for the Comment model

--- website/models.py
+++ website/models.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
+    # Ties all comments made by the User if any exist
+    comments = db.Relationship('Comment', backref='user', passive_deletes=True)
 
 
 class Post(db.Model):
@@ -18,3 +20,13 @@ class Post(db.Model):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     # Capture the currentUsers id to tie it to their created post
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    # Ties all comments made on a particular Post if any exist
+    comments = db.Relationship('Comment', backref='post', passive_deleted=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    # Ties individual comment to it's Post object
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
